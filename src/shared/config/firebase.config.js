a1f5bcc46e352be34ac7449501b67e7287f3f09e
Nem791 <phuongnam131201.nnnpn@gmail.com>
2023-05-24 09:30:42 +0700

    update

--- src/shared/config/firebase.config.js
+++ src/shared/config/firebase.config.js
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyAMoR9Vyjra7hc5GF82VOOmzHwu3v9qTgA",
-  authDomain: "nckh-card.firebaseapp.com",
-  projectId: "nckh-card",
-  storageBucket: "nckh-card.appspot.com",
-  messagingSenderId: "9842784329",
-  appId: "1:9842784329:web:373556be6ee5338e952798",
-  measurementId: "G-FJD4JBGYNG",
+  apiKey: "AIzaSyBzUSVGJnNVhJGUqXBVg1i73jODq92Z0TA",
+  authDomain: "flashcard-uploads.firebaseapp.com",
+  projectId: "flashcard-uploads",
+  storageBucket: "flashcard-uploads.appspot.com",
+  messagingSenderId: "58873052864",
+  appId: "1:58873052864:web:f10a52d712e2f72438f725",
+  measurementId: "G-HG5SXFN18Y",
 };
